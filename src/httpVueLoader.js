9c83302a03b12fb714eb2f3a06097945615de807
Franck Freiburger <franck.freiburger@gmail.com>
2017-02-14 10:44:45 +0100

    - report script section syntax errors (FireFox only)

--- src/httpVueLoader.js
+++ src/httpVueLoader.js
@@ -3,17 +3,18 @@ function httpVueLoader(url) {
 	return function(resolve, reject) {
 		
 		axios.get(url)
-		.then(function(vueFile) {
+		.then(function(res) {
 			
 			var template = '';
 			var module = { exports:{} };
 
 			var doc = document.implementation.createHTMLDocument('');
-			doc.body.innerHTML = vueFile.data;
+			doc.body.innerHTML = res.data;
 			var range = doc.createRange();
 			range.selectNodeContents(doc.body);
 			var fragment = range.extractContents();
-
+			
+			
 			for ( var it = fragment.firstChild; it; it = it.nextSibling ) {
 				
 				switch ( it.nodeName ) {
@@ -21,7 +22,20 @@ function httpVueLoader(url) {
 						template = it.innerHTML;
 						break;
 					case 'SCRIPT':
-						Function('module', it.textContent)(module);
+						try {
+							Function('module', it.textContent)(module);
+						} catch(ex) {
+							
+							if ( ex.lineNumber ) {
+								
+								var vueFileData = res.data.replace(/\r?\n/g, '\n');
+								var lineNumber = vueFileData.substr(0, vueFileData.indexOf(it.textContent)).split('\n').length + ex.lineNumber - 1;
+								throw new (ex.constructor)(ex.message, res.request.responseURL, lineNumber);
+							} else {
+								
+								throw ex;
+							}
+						}
 						break;
 					case 'STYLE':
 						var style = document.createElement('style');
